30b0c97e18b77809caafd0dd73fd3e12712a3e0f
Maycom Will <74081416+Maycomwill@users.noreply.github.com>
2024-02-22 21:26:46 -0300

    front end config

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex-1 h-screen flex w-full bg-slate-900 text-zinc-50 items-center justify-center">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
