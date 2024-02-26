7c8c5aae97943b5c03d9bdfcdb4b6bf6f2a89a48
Maycom Will <74081416+Maycomwill@users.noreply.github.com>
2024-02-26 19:02:50 -0300

    build hotfix

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
