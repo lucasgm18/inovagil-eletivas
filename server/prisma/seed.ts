bfb97f70885831ae51173f719e8c7e5521c42d20
Maycom Willams <74081416+Maycomwill@users.noreply.github.com>
2024-03-01 18:43:28 -0300

    fix: seed corrigido

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -24,103 +24,105 @@ async function main() {
       // Here log the result array
       console.log("parsed csv data:");
       console.log(data);
+      await prisma.alunosMatriculados.deleteMany();
+      await prisma.students.deleteMany();
+      await prisma.classes.deleteMany();
       await prisma.students.createMany({
         data,
       });
+      await prisma.classes.createMany({
+        data: [
+          {
+            nome: "Educação financeira",
+            professor: "PAULO HERTON",
+            serie: "1",
+          },
+          {
+            nome: "Iniciação Científica",
+            professor: "Daniel",
+            serie: "1",
+          },
+          {
+            nome: "Esportes Alternativos",
+            professor: "Diego",
+            serie: "1",
+          },
+          {
+            nome: "Natureza na Prática",
+            professor: "Fabíola",
+            serie: "1",
+          },
+          {
+            nome: "BIOEMFOCO",
+            professor: "Fabíola",
+            serie: "2",
+            diaDaSemana: "TERCA",
+          },
+          {
+            nome: "Histórias em Quadrinhos",
+            professor: "Franci",
+            serie: "2",
+            diaDaSemana: "TERCA",
+          },
+          {
+            nome: "Historicidade da Cultura Corporal",
+            professor: "Eli",
+            serie: "2",
+            diaDaSemana: "TERCA",
+          },
+          {
+            nome: "Morfologia Vegetal",
+            professor: "Renato",
+            serie: "2",
+            diaDaSemana: "TERCA",
+          },
+          {
+            nome: "Natureza na Prática",
+            professor: "Fabiola",
+            serie: "2",
+            diaDaSemana: "QUINTA",
+          },
+          {
+            nome: "Práticas de Laboratório",
+            professor: "Renato",
+            serie: "2",
+            diaDaSemana: "QUINTA",
+          },
+          {
+            nome: "Iniciação aos Fundamentos do Basquete",
+            professor: "Eli",
+            serie: "2",
+            diaDaSemana: "QUINTA",
+          },
+          {
+            nome: "PNL e as Técnicas de Vendas",
+            professor: "Alex",
+            serie: "2",
+            diaDaSemana: "QUINTA",
+          },
+          {
+            nome: "Explorando Soluções Estratégicas",
+            professor: "Magnun",
+            serie: "3",
+          },
+          {
+            nome: "Resolução de Questões",
+            professor: "Paulo",
+            serie: "3",
+          },
+          {
+            nome: "Revendo a Física de Olho no Enem",
+            professor: "Carlos",
+            serie: "3",
+          },
+          {
+            nome: "Iniciação ao Futsal",
+            professor: "Diego",
+            serie: "3",
+          },
+        ],
+      });
     });
-
-  await prisma.classes.createMany({
-    data: [
-      {
-        nome: "Educação financeira",
-        professor: "PAULO HERTON",
-        serie: "1",
-      },
-      {
-        nome: "Iniciação Científica",
-        professor: "Daniel",
-        serie: "1",
-      },
-      {
-        nome: "Esportes Alternativos",
-        professor: "Diego",
-        serie: "1",
-      },
-      {
-        nome: "Natureza na Prática",
-        professor: "Fabíola",
-        serie: "1",
-      },
-      {
-        nome: "BIOEMFOCO",
-        professor: "Fabíola",
-        serie: "2",
-        diaDaSemana: "TERCA",
-      },
-      {
-        nome: "Histórias em Quadrinhos",
-        professor: "Franci",
-        serie: "2",
-        diaDaSemana: "TERCA",
-      },
-      {
-        nome: "Historicidade da Cultura Corporal",
-        professor: "Eli",
-        serie: "2",
-        diaDaSemana: "TERCA",
-      },
-      {
-        nome: "Morfologia Vegetal",
-        professor: "Renato",
-        serie: "2",
-        diaDaSemana: "TERCA",
-      },
-      {
-        nome: "Natureza na Prática",
-        professor: "Fabiola",
-        serie: "2",
-        diaDaSemana: "QUINTA",
-      },
-      {
-        nome: "Práticas de Laboratório",
-        professor: "Renato",
-        serie: "2",
-        diaDaSemana: "QUINTA",
-      },
-      {
-        nome: "Iniciação aos Fundamentos do Basquete",
-        professor: "Eli",
-        serie: "2",
-        diaDaSemana: "QUINTA",
-      },
-      {
-        nome: "PNL e as Técnicas de Vendas",
-        professor: "Alex",
-        serie: "2",
-        diaDaSemana: "QUINTA",
-      },
-      {
-        nome: "Explorando Soluções Estratégicas",
-        professor: "Magnun",
-        serie: "3",
-      },
-      {
-        nome: "Resolução de Questões",
-        professor: "Paulo",
-        serie: "3",
-      },
-      {
-        nome: "Revendo a Física de Olho no Enem",
-        professor: "Carlos",
-        serie: "3",
-      },
-      {
-        nome: "Iniciação ao Futsal",
-        professor: "Diego",
-        serie: "3",
-      },
-    ],
-  });
 }
 
 main()
